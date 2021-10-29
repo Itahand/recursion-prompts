79ebd340416e19168e50ff4b7987e0c7091b29ff
Itahand <Itahand.naizir@gmail.com>
2021-10-29 16:32:21 -0400

    Count keys and count values

--- src/recursion.js
+++ src/recursion.js
@@ -326,6 +326,16 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
+  let result = 0;
+  for (let i in obj){
+    if(i === key){
+      result += 1;
+    }
+    if (obj[i] instanceof Object){
+      result += countKeysInObj(obj[i], key);
+    }
+  }
+  return result;
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
@@ -333,6 +343,16 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  let result = 0;
+  for (let i in obj){
+    if(obj[i] === value){
+      result += 1;
+    }
+    if (obj[i] instanceof Object){
+      result += countValuesInObj(obj[i], value);
+    }
+  }
+  return result;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
